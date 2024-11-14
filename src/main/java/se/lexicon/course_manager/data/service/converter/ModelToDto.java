e2a682548bed87634c6ebeab572198f8ff44d2b4
Patrik Alm <patrikalm@github.io>
2024-11-14 08:03:35 +0100

    Continued to implement ModelToDto.

--- src/main/java/se/lexicon/course_manager/data/service/converter/ModelToDto.java
+++ src/main/java/se/lexicon/course_manager/data/service/converter/ModelToDto.java
@@ -1,6 +1,7 @@
 package se.lexicon.course_manager.data.service.converter;
 
 import org.springframework.stereotype.Component;
+import se.lexicon.course_manager.data.dao.CourseCollectionRepository;
 import se.lexicon.course_manager.dto.views.CourseView;
 import se.lexicon.course_manager.dto.views.StudentView;
 import se.lexicon.course_manager.model.Course;
@@ -8,6 +9,8 @@ import se.lexicon.course_manager.model.Student;
 
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 // TODO Convert model -> view & models -> views
@@ -28,7 +31,19 @@ public class ModelToDto implements Converters {
 
     @Override
     public List<CourseView> coursesToCourseViews(Collection<Course> courses) {
-        return null;
+
+        List<CourseView> courseViews = null;
+
+        Iterator<Course> itr = courses.iterator();
+
+        while (itr.hasNext()) {
+
+            //TODO add null pointer check
+
+            courseViews.add(new CourseView(itr.next().getId(), itr.next().getCourseName(), itr.next().getStartDate(), itr.next().getWeekDuration(), studentsToStudentViews(itr.next().getStudents())));
+
+        }
+        return courseViews;
     }
 
     @Override
